dd01c0a808ec759306585e3d36710afea3f32f6d
Angel Alexander <angale22@gmail.com>
2020-07-24 17:01:50 -0500

    created index, style, and script files

---
+++ script.js
@@ -0,0 +1,13 @@
+var startBtn = document.querySelector("#start");
+var startContainer = document.querySelector(".start-container");
+var firstQuestionCont = document.querySelector(".question-1");
+
+startBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    if(event.target.matches("#start")) {
+        startContainer.setAttribute("style", "display:none");
+        firstQuestionCont.setAttribute("style", "visibility:visible");
+    }
+});
+
+var 
