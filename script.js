337bf8754d62af80902f102cac8076799780cc66
Angel Alexander <angale22@gmail.com>
2020-07-28 17:25:08 -0500

    show main menu function

--- script.js
+++ script.js
@@ -45,3 +45,48 @@ submit_initials.addEventListener("click", OnClick_SubmitInitials);
 
 btn_highscroes_back.addEventListener("click", ShowMainMenu);
 btn_highscores_clear.addEventListener("click", OnClick_ClearHighscores);
+
+ShowMainMenu();
+
+
+///
+///
+///
+///
+
+
+function ShowMainMenu()
+{
+  document.getElementById("div_main").setAttribute("style", "display:block");
+  document.getElementById("div_question").setAttribute("style", "display:none");
+  document.getElementById("div_feedback").setAttribute("style", "display:block");
+  document.getElementById("div_submitScore").setAttribute("style", "display:none");
+  document.getElementById("div_highscoreTable").setAttribute("style", "display:none");
+}
+
+function ShowQuestion(index)
+{
+    // clear any options from previous question
+    answerOptionsElem.innerHTML = "";
+
+    // make sure we have a valid question
+    if(questions[index] === undefined) {
+        EndOfQuiz();
+    } else {
+    //var fragment = document.createDocumentFragment(); // change later to something more basic bitch
+
+    // set question
+    questionElem.innerHTML = questions[index].question;
+
+    
+    var indexCounter = 0;
+    questions[index].choices.forEach(function(choice) {
+        var tempBtn = document.createElement('button');
+        tempBtn.innerHTML = choice;
+        tempBtn.setAttribute("onclick", "SubmitAnswer(" + indexCounter + ")");
+        tempBtn.classList.add("btn", "btn-block", "btn-light");
+        answerOptionsElem.appendChild(tempBtn);
+        indexCounter++;
+    });
+    }
+}
