bd780ef64ecf4449ee9d6ed734c2a13e8213ef67
Angel Alexander <angale22@gmail.com>
2020-07-28 17:20:14 -0500

    start over - added html and questions script

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-var startBtn = document.querySelector("#start");
-var startContainer = document.querySelector(".start-container");
-var firstQuestionCont = document.querySelector(".question-1");
-var secondQuestionCont = document.querySelector(".question-2");
-var thirdQuestionCont = document.querySelector(".question-3");
-var fourthQuestionCont = document.querySelector(".question-4");
-var fifthQuestionCont = document.querySelector(".question-5");
-
-startBtn.addEventListener("click", function(event) {
-    event.preventDefault();
-    if(event.target.matches("#start")) {
-    startContainer.setAttribute("style", "display:none");
-    firstQuestionCont.setAttribute("style", "visibility:visible");
-    }
- });
+// Question Array ARRAY
+var questions =
+    [
+      {
+        question: "Select 2",
+        choices: ["One", "Two", "Four"],
+        answer: 1
+      },
+      {
+        question: "Select Dog",
+        choices: ["Cat", "Monkey", "Dog", "lol"],
+        answer: 2
+      },
+      {
+        question: "2+2",
+        choices: ["4", "1", "17", "0"],
+        answer: 0
+      }
+    ];
